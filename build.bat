ba131505394c9ae2939d482596c62163b6dc6ef6
Matthew <spoonerm12@gmail.com>
2018-01-27 14:55:14 -0500

    Made path names general

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 mkdir .\bin
 mkdir .\out
-"C:\Program Files\Java\jdk1.8.0_131\bin\javac.exe" -d ./bin -cp src src/org/spooner/java/BunnyClick/*.java
+javac.exe -d ./bin -cp src src/org/spooner/java/BunnyClick/*.java
 if %errorlevel% NEQ 0 (goto done)
-"C:\Program Files\Java\jdk1.8.0_131\bin\jar.exe" cvfm ./out/BunnyClick.jar ./build-files/game-manifest.MF -C bin org buildings achievements sounds sprites upgrades *.xml *png font.ttf
-xcopy /i /y .\build-files\run.bat .\out\run.bat
+jar.exe cvfm ./out/BunnyClick.jar ./build-files/game-manifest.MF -C bin org buildings achievements sounds sprites upgrades *.xml *png font.ttf
+echo f | xcopy /i /y .\build-files\run.bat .\out\run.bat
 :done
 PAUSE
